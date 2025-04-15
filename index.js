e33a9903ee30774b603bfac32fc82a8f6a3e6a6d
eeescamillar <eeescamillar@libertadores.edu.co>
2025-04-14 19:47:36 -0500

    metodo GET USER y variables env

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 require('dotenv').config();
+const express = require('express');
+const userRouter = require('./routes/UserRoutes');
 
-// Middleware para parsear JSON y URL-encoded bodies
-app.use(express.urlencoded({ extended: true }));
+const app = express();
 app.use(express.json());
 
-// Importar las rutas
-const encuentroRoutes = require('./routes/encuentroRoutes');
-const comentarioRoutes = require('./routes/comentarioRoutes');
-const parejaRoutes = require('./routes/parejaRoutes'); // Si tienes rutas para pareja
-const planRoutes = require('./routes/planRoutes'); // Si tienes rutas para plan
-const userRoutes = require('./routes/userRoutes'); // Si tienes rutas para usuario
+// Usar rutas
+app.use('/users', userRouter);
 
-// Usar las rutas
-app.use('/api', encuentroRoutes);
-app.use('/api', comentarioRoutes);
-app.use('/api', parejaRoutes); // Si tienes rutas para pareja
-app.use('/api', planRoutes); // Si tienes rutas para plan
-app.use('/api', userRoutes); // Si tienes rutas para usuario
-
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor en puerto ${PORT}`);
 });
