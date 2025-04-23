cbb1bf3793fa6e57dfa520c5818eb85da2f48905
eeescamillar <eeescamillar@libertadores.edu.co>
2025-04-23 17:26:03 -0500

    get parejas

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const userRouter = require('./routes/UserRoutes');
+const parejaRouter = require('./routes/ParejaRoutes.js')
 
 const app = express();
 app.use(express.json());
 
 // Usar rutas
 app.use('/users', userRouter);
+app.use('/pareja', parejaRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
